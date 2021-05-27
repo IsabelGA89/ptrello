abc378c58841861e6be05f03aef075f9b46cc01a
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-27 22:37:39 +0200

    login background fixex

--- pages/auth/login.php
+++ pages/auth/login.php
@@ -57,7 +57,7 @@ if(isset($_POST['login'])) {
 </head>
 
 <body>
-<div class="flex w-screen h-screen text-gray-400 bg-gray-900">
+
     <section class="min-h-screen flex items-stretch text-white ">
     <div class="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center"
          style="background-image: url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);">
@@ -149,7 +149,7 @@ if(isset($_POST['login'])) {
         </div>
     </div>
 </section>
-</div>
+
 </body>
 
 <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
