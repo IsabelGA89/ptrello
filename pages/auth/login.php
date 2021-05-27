49e1f1af7cf8cdbd6ec4f0d650732f240129b247
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-27 22:32:53 +0200

    Update login.php

--- pages/auth/login.php
+++ pages/auth/login.php
@@ -106,7 +106,7 @@ if(isset($_POST['login'])) {
             <p class="text-gray-100">
                 Identificate
             </p>
-            <form method="post" action="../index.php" name="signin-form" class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
+            <form method="post" action="./index.php" name="signin-form" class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
                 <div class="pb-2 pt-4">
                     <input type="text" name="username" pattern="[a-zA-Z0-9]+" required placeholder="Username"
                            class="block w-full p-4 text-lg rounded-sm bg-black">
