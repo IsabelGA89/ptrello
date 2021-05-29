902dc947fda014500b0b4927b315ec927fdcc3b3
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-29 10:06:57 +0200

    Update testingLogin.php

--- testing/testingLogin.php
+++ testing/testingLogin.php
@@ -1,13 +1,13 @@
 <?php
+
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
+
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
@@ -16,12 +16,13 @@ if (mysqli_connect_errno()) {
     exit();
 }
 $consulta ="select * from users where username='isa'";
+$arr_info =[];
 
 if ($resultado = $conn->query($consulta)) {
 
     /* obtener el array de objetos */
     while ($obj = $resultado->fetch_array()) {
-        echo($obj);
+        var_dump($obj);
     }
     /* liberar el conjunto de resultados */
     $resultado->close();
