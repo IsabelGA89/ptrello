0df70f2332323e45f5562e161cad7ac3f5e3b68b
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-29 19:36:05 +0200

    Update account.php

--- pages/account.php
+++ pages/account.php
@@ -6,8 +6,8 @@ session_start();
     exit;
 }*/
 
-$email="ejemplo@gmail.com";
-$username="ejemplo nick";
+$email = "ejemplo@gmail.com";
+$username = "ejemplo nick";
 ?>
 <!doctype html>
 <html lang="es">
@@ -64,12 +64,13 @@ $username="ejemplo nick";
     <div class="flex flex-col flex-grow">
         <!--Header-->
         <div class="flex items-center flex-shrink-0 h-16 px-8 border-b border-gray-800">
-            <h1 class="text-lg font-medium"><h1 style="text-align: center;"><i class="fab fa-trello text-primary"></i>
+            <h1 class="text-3xl font-medium"><h1 style="text-align: center;"><i class="fab fa-trello text-primary"></i>
                     Consultas a tableros de Trello</h1>
                 <form action="auth/login.php" method="post"
-                      class="flex btn items-center justify-center h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-800">
-                    <i class="fas fa-sign-out-alt fa-lg"></i><input type="submit" name="logout" value="Log out"
-                                                                    class="flex btn items-center justify-center h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-800"/>
+                      class="flex btn items-center justify-center h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-400">
+                    <i class="fas fa-sign-out-alt fa-lg"></i>
+                    <input type="submit" name="logout" value="Log out"
+                           class="flex btn items-center justify-center text-gray-400  h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-400"/>
                 </form>
         </div>
         <!--Body-->
@@ -77,60 +78,79 @@ $username="ejemplo nick";
             <div class="bg-cover bg-center border rounded-t-lg shadow-lg"
                  style="background-image: url(https://images.unsplash.com/photo-1572817519612-d8fadd929b00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80)">
 
-                <section class="py-40 bg-gray-100 bg-opacity-50 h-screen">
+                <section class="py-40 bg-gray-100  bg-opacity-50 h-screen">
                     <div class="mx-auto container max-w-2xl md:w-3/4 shadow-md">
-                        <div class="bg-white space-y-6">
-                            <!--Imagen y nombre-->
-                            <div class="bg-gray-100 p-4 border-t-2 bg-opacity-5 border-indigo-400 rounded-t">
-                                <div class="max-w-sm mx-auto md:w-full justify-items-center md:mx-0">
-                                    <div class="inline-flex items-center space-x-4">
-                                        <img
-                                                class="w-10 h-10 object-cover rounded-full"
-                                                alt="User avatar"
-                                                src="../img/unnamed.png"
-                                        />
-
-                                        <h1 class="text-gray-600 ml-2"><?php echo $username ?></h1>
-                                    </div>
+                        <!-- Icono y nombre-->
+                        <div class="bg-gray-100 p-4 border-t-2 bg-opacity-5 border-indigo-400 rounded-t">
+                            <div class="max-w-sm mx-auto md:w-full md:mx-0">
+                                <div class="inline-flex items-center space-x-4">
+                                    <i class="fas fa-user-circle"></i>
+                                    <h1 class="text-gray-600"><?= $username ?></h1>
                                 </div>
                             </div>
-                            <hr/>
-                            <!--Info cuenta-->
+                        </div>
+                        <!--Cuenta-->
+                        <div class="bg-white space-y-6">
                             <div class="md:inline-flex space-y-4 md:space-y-0 w-full p-4 text-gray-500 items-center">
-                                <h2 class="md:w-1/3 max-w-sm mx-auto">Información de la cuenta</h2>
+                                <h2 class="md:w-1/3 max-w-sm mx-auto">Cuenta</h2>
                                 <!--Email-->
                                 <div class="md:w-2/3 max-w-sm mx-auto">
                                     <label class="text-sm text-gray-400">Email</label>
                                     <div class="w-full inline-flex border">
                                         <div class="pt-2 w-1/12 bg-gray-100 bg-opacity-50">
-                                            <i class="fas fa-envelope"></i>
+                                            <svg
+                                                    fill="none"
+                                                    class="w-6 text-gray-400 mx-auto"
+                                                    viewBox="0 0 24 24"
+                                                    stroke="currentColor"
+                                            >
+                                                <path
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        stroke-width="2"
+                                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                                                />
+                                            </svg>
                                         </div>
                                         <input
                                                 type="email"
                                                 class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
-                                                placeholder="<?php echo $email ?>"
+                                                placeholder="<?= $email ?>"
                                                 disabled
                                         />
                                     </div>
                                 </div>
                                 <!--Username-->
-                                <div class="md:w-2/3 mx-auto max-w-sm space-y-5">
-                                    <div>
-                                        <label class="text-sm text-gray-400">Username</label>
-                                        <div class="w-full inline-flex border">
-                                            <div class="w-1/12 pt-2 bg-gray-100">
-                                                <i class="fas fa-user"></i>
-                                            </div>
-                                            <input
-                                                    type="text"
-                                                    class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
-                                                    placeholder="<?php echo $username ?>"
-                                            />
+                                <div class="md:w-2/3 max-w-sm mx-auto">
+
+                                    <label class="text-sm text-gray-400">Username</label>
+                                    <div class="w-full inline-flex border">
+                                        <div class="pt-2 w-1/12 bg-gray-100 bg-opacity-50">
+                                            <svg
+                                                    fill="none"
+                                                    class="w-6 text-gray-400 mx-auto"
+                                                    viewBox="0 0 24 24"
+                                                    stroke="currentColor"
+                                            >
+                                                <path
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        stroke-width="2"
+                                                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                                                />
+                                            </svg>
                                         </div>
+                                        <input
+                                                type="text"
+                                                class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                                                placeholder="<?= $username ?>"
+                                        />
                                     </div>
+
                                 </div>
                             </div>
-                            <hr />
+
+                            <hr/>
                            <!-- Cambiar contraseña-->
                             <div class="md:inline-flex w-full space-y-4 md:space-y-0 p-8 text-gray-500 items-center">
                                 <h2 class="md:w-4/12 max-w-sm mx-auto">Cambiar contraseña</h2>
@@ -138,16 +158,29 @@ $username="ejemplo nick";
                                 <div class="md:w-5/12 w-full md:pl-9 max-w-sm mx-auto space-y-5 md:inline-flex pl-2">
                                     <div class="w-full inline-flex border-b">
                                         <div class="w-1/12 pt-2">
-                                            <i class="fas fa-lock"></i>
+                                            <svg
+                                                    fill="none"
+                                                    class="w-6 text-gray-400 mx-auto"
+                                                    viewBox="0 0 24 24"
+                                                    stroke="currentColor"
+                                            >
+                                                <path
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        stroke-width="2"
+                                                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                                                />
+                                            </svg>
                                         </div>
                                         <input
+                                                name="new_pass"
                                                 type="password"
                                                 class="w-11/12 focus:outline-none focus:text-gray-600 p-2 ml-4"
-                                                placeholder="Nueva contraseña"
+                                                placeholder="Nueva"
                                         />
                                     </div>
                                 </div>
-
+                               <!-- Boton actualizar-->
                                 <div class="md:w-3/12 text-center md:pl-6">
                                     <button class="text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-4 inline-flex items-center focus:outline-none md:float-right">
                                         <svg
@@ -163,17 +196,29 @@ $username="ejemplo nick";
                                                     d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
                                             />
                                         </svg>
-                                        Update
+                                        Actualizar
                                     </button>
                                 </div>
                             </div>
 
-                            <hr />
-                            <!--Eliminar cuenta-->
+                            <hr/>
+                            <!--ELiminar cuenta-->
                             <div class="w-full p-4 text-right text-gray-500">
                                 <button class="inline-flex items-center focus:outline-none mr-4">
-                                    <i class="fas fa-trash-alt"></i>
-                                     Eliminar cuenta
+                                    <svg
+                                            fill="none"
+                                            class="w-4 mr-2"
+                                            viewBox="0 0 24 24"
+                                            stroke="currentColor"
+                                    >
+                                        <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                        />
+                                    </svg>
+                                    Borrar cuenta
                                 </button>
                             </div>
                         </div>
