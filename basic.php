ad71e1287647e202743fdafd9d90eddb2002ca5a
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-29 08:32:17 +0200

    Update basic.php

--- basic.php
+++ basic.php
@@ -2,7 +2,7 @@
 /*DB url   mysql://b0a56ad1436acf:64c5235e@us-cdbr-east-03.cleardb.com/heroku_4edea3226fe2494?reconnect=true*/
 
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
+var_dump($cleardb_url);
 
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
