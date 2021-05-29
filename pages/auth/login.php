a8ffc50adec7234cd9030959d59811b0d821780e
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-29 18:16:32 +0200

    Update login.php

--- pages/auth/login.php
+++ pages/auth/login.php
@@ -10,7 +10,7 @@ $info="";
 
 
 if(isset($_GET['info'])){
-    $msj = $_GET['info'];
+    $info = $_GET['info'];
 }
 
 if(isset($_POST['login'])) {
@@ -73,29 +73,6 @@ if(isset($_POST['login'])) {
 <?php
 if($error != ""){
 ?>
-    <?php
-    if ($info != "") {
-        ?>
-        <!--Msj section-->
-        <div class="block text-sm text-blue-600 bg-blue-200 border border-blue-400 h-12 flex items-center p-4 rounded-sm relative"
-             role="alert">
-          <span class="mr-1">
-            <svg class="fill-current text-blue-500 inline-block h-5 w-5" xmlns="http://www.w3.org/2000/svg"
-                 viewBox="0 0 24 24" width="24" height="24">
-              <path class="heroicon-ui"
-                    d="M15 19a3 3 0 0 1-6 0H4a1 1 0 0 1 0-2h1v-6a7 7 0 0 1 4.02-6.34 3 3 0 0 1 5.96 0A7 7 0 0 1 19 11v6h1a1 1 0 0 1 0 2h-5zm-4 0a1 1 0 0 0 2 0h-2zm0-12.9A5 5 0 0 0 7 11v6h10v-6a5 5 0 0 0-4-4.9V5a1 1 0 0 0-2 0v1.1z"/>
-            </svg>
-          </span> <?php
-            if ($info != "") {
-                echo $info;
-            } ?>
-            <span>
-
-          </span>
-        </div>
-        <?php
-    }
-    ?>
 <!--Msj section-->
 <div class="block text-sm text-red-600 bg-red-200 border border-red-400 h-12 flex items-center p-4 rounded-sm relative" role="alert">
           <span class="mr-1">
@@ -111,6 +88,29 @@ if($error != ""){
           </span>
 </div>
 <?php
+}
+?>
+<?php
+if ($info != "") {
+    ?>
+    <!--Msj section-->
+    <div class="block text-sm text-blue-600 bg-blue-200 border border-blue-400 h-12 flex items-center p-4 rounded-sm relative"
+         role="alert">
+          <span class="mr-1">
+            <svg class="fill-current text-blue-500 inline-block h-5 w-5" xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 24 24" width="24" height="24">
+              <path class="heroicon-ui"
+                    d="M15 19a3 3 0 0 1-6 0H4a1 1 0 0 1 0-2h1v-6a7 7 0 0 1 4.02-6.34 3 3 0 0 1 5.96 0A7 7 0 0 1 19 11v6h1a1 1 0 0 1 0 2h-5zm-4 0a1 1 0 0 0 2 0h-2zm0-12.9A5 5 0 0 0 7 11v6h10v-6a5 5 0 0 0-4-4.9V5a1 1 0 0 0-2 0v1.1z"/>
+            </svg>
+          </span> <?php
+        if ($info != "") {
+            echo $info;
+        } ?>
+        <span>
+
+          </span>
+    </div>
+    <?php
 }
 ?>
     <section class="min-h-screen flex items-stretch text-white ">
