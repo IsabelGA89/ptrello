82cf6429b0a90b6cd07ce1f3c58c98d8c52f4064
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-29 21:46:49 +0200

    500 error

---
+++ pages/errors/500.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>500 Error</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="error-500" data-text="Oh no! Nuestro código espaguetti no está funcionando adecuadamente. Volveremos pronto!">
+    <spaguetti>
+    <fork></fork>
+    <meat></meat>
+    <pasta></pasta>
+    <plate></plate>
+  </spaguetti>
+</div>
+<!-- partial -->
+  <script  src="./script.js"></script>
+
+</body>
+</html>
