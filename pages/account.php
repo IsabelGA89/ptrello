115484d8841a861f3b665a99a1e53c587f29566f
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-29 19:41:41 +0200

    Update account.php

--- pages/account.php
+++ pages/account.php
@@ -151,7 +151,7 @@ $username = "ejemplo nick";
                             </div>
 
                             <hr/>
-                           <!-- Cambiar contraseña-->
+                            <!-- Cambiar contraseña-->
                             <div class="md:inline-flex w-full space-y-4 md:space-y-0 p-8 text-gray-500 items-center">
                                 <h2 class="md:w-4/12 max-w-sm mx-auto">Cambiar contraseña</h2>
 
@@ -180,31 +180,19 @@ $username = "ejemplo nick";
                                         />
                                     </div>
                                 </div>
-                               <!-- Boton actualizar-->
+                                <!-- Boton actualizar-->
                                 <div class="md:w-3/12 text-center md:pl-6">
                                     <button class="text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-4 inline-flex items-center focus:outline-none md:float-right">
-                                        <svg
-                                                fill="none"
-                                                class="w-4 text-white mr-2"
-                                                viewBox="0 0 24 24"
-                                                stroke="currentColor"
-                                        >
-                                            <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    stroke-width="2"
-                                                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-                                            />
-                                        </svg>
-                                        Actualizar
+                                        <i class="fas fa-sync mr-2"></i>
+                                          Actualizar
                                     </button>
                                 </div>
                             </div>
 
                             <hr/>
                             <!--ELiminar cuenta-->
-                            <div class="w-full p-4 text-right text-gray-500">
-                                <button class="inline-flex items-center focus:outline-none mr-4">
+                            <div class="w-full p-4 text-right text-gray-500 ">
+                                <button class="inline-flex items-center focus:outline-none mr-4 hover:text-red-400">
                                     <svg
                                             fill="none"
                                             class="w-4 mr-2"
