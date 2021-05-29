771c32664a33ccb321bb1fe206016a8fddc520e1
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-29 13:37:43 +0200

    Update report.php

--- pages/report.php
+++ pages/report.php
@@ -309,6 +309,8 @@ if (isset($_POST['logout'])) {
                                             <svg class="h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2c-.8 0-1.5.7-1.5 1.5v.688C7.344 4.87 5 7.62 5 11v4.5l-2 2.313V19h18v-1.188L19 15.5V11c0-3.379-2.344-6.129-5.5-6.813V3.5c0-.8-.7-1.5-1.5-1.5zm-2 18c0 1.102.898 2 2 2 1.102 0 2-.898 2-2z"/></svg>
                                         </button>
                                         <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
+                                    </div>
+                                </div>
                                     <?php
                                     if ($data != null || $data != "") {
                                         if (isset($arr_cards) && $arr_cards != "") {
@@ -323,8 +325,7 @@ if (isset($_POST['logout'])) {
                                             echo "</div>";
                                         }
                                         ?>
-                                    </div>
-                                </div>
+
                                         <?php
                                         echo "<hr/>";
                                         echo "<div class='container' id='select-board-section'>";
