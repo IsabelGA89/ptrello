5dd2aa0e150337e9b8f2a4c7cc802ff168d93f30
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-30 08:48:08 +0200

    Testing and account

--- testing/components.php
+++ testing/components.php
@@ -1,34 +1,6 @@
 <?php
-require "../Class/Heroku.php";
-
-session_start();
-/*if ((!$_SESSION['user_id'])) {
-    $login = "./pages/auth/login.php";
-    header("Location: $login");
-    exit;
-}*/
-$info="";
-$error="";
-
-
-//0.5º obtenemos el id del user
-$bd_id = $_SESSION['user_id'];
-//1º consulta datos en bd
-$conn = new Heroku;
-$consulta = "select * from users where id='$bd_id'";
-
-if ($resultado = $conn->query($consulta)) {
-    $arr_info = $resultado->fetch_array();
-    $resultado->close();
-}
-/* cerrar la conexión */
-$conn->close();
-
-
-$email = $arr_info['email'];
-$username = $arr_info['username'];
-
 ?>
+
 <!doctype html>
 <html lang="es">
 <head>
@@ -54,53 +26,6 @@ $username = $arr_info['username'];
 </head>
 
 <body class="h-screen overflow-hidden flex items-center justify-center" style="background: #edf2f7;">
-<?php
-if ($error != "") {
-    ?>
-    <!--Msj section-->
-    <div class="block text-sm text-red-600 bg-red-200 border border-red-400 h-12 flex items-center p-4 rounded-sm relative"
-         role="alert">
-          <span class="mr-1">
-            <svg class="fill-current text-red-500 inline-block h-5 w-5" xmlns="http://www.w3.org/2000/svg"
-                 viewBox="0 0 24 24" width="24" height="24">
-              <path class="heroicon-ui"
-                    d="M15 19a3 3 0 0 1-6 0H4a1 1 0 0 1 0-2h1v-6a7 7 0 0 1 4.02-6.34 3 3 0 0 1 5.96 0A7 7 0 0 1 19 11v6h1a1 1 0 0 1 0 2h-5zm-4 0a1 1 0 0 0 2 0h-2zm0-12.9A5 5 0 0 0 7 11v6h10v-6a5 5 0 0 0-4-4.9V5a1 1 0 0 0-2 0v1.1z"/>
-            </svg>
-          </span>
-        <span>
-           <?php
-           if ($error != "") {
-               echo $error;
-           } ?>
-          </span>
-    </div>
-    <?php
-}
-?>
-
-<?php
-if ($info != "") {
-    ?>
-    <!--Msj section-->
-    <div class="block text-sm text-blue-600 bg-blue-200 border border-blue-400 h-12 flex items-center p-4 rounded-sm relative"
-         role="alert">
-          <span class="mr-1">
-            <svg class="fill-current text-blue-500 inline-block h-5 w-5" xmlns="http://www.w3.org/2000/svg"
-                 viewBox="0 0 24 24" width="24" height="24">
-              <path class="heroicon-ui"
-                    d="M15 19a3 3 0 0 1-6 0H4a1 1 0 0 1 0-2h1v-6a7 7 0 0 1 4.02-6.34 3 3 0 0 1 5.96 0A7 7 0 0 1 19 11v6h1a1 1 0 0 1 0 2h-5zm-4 0a1 1 0 0 0 2 0h-2zm0-12.9A5 5 0 0 0 7 11v6h10v-6a5 5 0 0 0-4-4.9V5a1 1 0 0 0-2 0v1.1z"/>
-            </svg>
-          </span> <?php
-        if ($info != "") {
-            echo $info;
-        } ?>
-        <span>
-
-          </span>
-    </div>
-    <?php
-}
-?>
 <div class="flex w-screen h-screen text-gray-400 bg-gray-900">
     <!--Navbar-->
     <div class="flex flex-col items-center w-16 pb-4 overflow-auto border-r border-gray-800 text-gray-500">
@@ -152,7 +77,7 @@ if ($info != "") {
                             <div class="max-w-sm mx-auto md:w-full md:mx-0">
                                 <div class="inline-flex items-center space-x-4">
                                     <i class="fas fa-user-circle fa-7x color:indigo"></i>
-                                    <h1 class="uppercase text-gray-600 font-black"><?= $username ?></h1>
+                                    <h1 class="uppercase text-gray-600 font-black"><?= $username ?? null ?></h1>
                                 </div>
                             </div>
                         </div>
@@ -166,52 +91,52 @@ if ($info != "") {
                                     <div class="w-full inline-flex border">
                                         <div class="pt-2 w-1/12 bg-gray-100 bg-opacity-50">
                                             <svg
-                                                    fill="none"
-                                                    class="w-6 text-gray-400 mx-auto"
-                                                    viewBox="0 0 24 24"
-                                                    stroke="currentColor"
+                                                fill="none"
+                                                class="w-6 text-gray-400 mx-auto"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
                                             >
                                                 <path
-                                                        stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                                                 />
                                             </svg>
                                         </div>
                                         <input
-                                                type="email"
-                                                class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
-                                                placeholder="<?= $email ?>"
-                                                disabled
+                                            type="email"
+                                            class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                                            placeholder="<?= $email ?? null?>"
+                                            disabled
                                         />
                                     </div>
                                 </div>
-                                <form action="account.php" method="post">
                                 <!--Username-->
                                 <div class="md:w-2/3 max-w-sm mx-auto">
+
                                     <label class="text-sm text-gray-400">Username</label>
                                     <div class="w-full inline-flex border">
                                         <div class="pt-2 w-1/12 bg-gray-100 bg-opacity-50">
                                             <svg
-                                                    fill="none"
-                                                    class="w-6 text-gray-400 mx-auto"
-                                                    viewBox="0 0 24 24"
-                                                    stroke="currentColor"
+                                                fill="none"
+                                                class="w-6 text-gray-400 mx-auto"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
                                             >
                                                 <path
-                                                        stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                                                 />
                                             </svg>
                                         </div>
                                         <input
                                                 name="new_username"
-                                                type="text"
-                                                class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
-                                                placeholder="<?= $username ?>"
+                                            type="text"
+                                            class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
+                                            placeholder="<?= $username ?>"
                                         />
                                     </div>
 
@@ -222,39 +147,39 @@ if ($info != "") {
                             <!-- Cambiar contraseña-->
                             <div class="md:inline-flex w-full space-y-4 md:space-y-0 p-8 text-gray-500 items-center">
                                 <h2 class="md:w-4/12 max-w-sm mx-auto">Cambiar contraseña</h2>
-                                    <div class="md:w-5/12 w-full md:pl-9 max-w-sm mx-auto space-y-5 md:inline-flex pl-2">
-                                        <div class="w-full inline-flex border-b">
-                                            <div class="w-1/12 pt-2">
-                                                <svg
-                                                        fill="none"
-                                                        class="w-6 text-gray-400 mx-auto"
-                                                        viewBox="0 0 24 24"
-                                                        stroke="currentColor"
-                                                >
-                                                    <path
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                                                    />
-                                                </svg>
-                                            </div>
-                                            <input
-                                                    name="new_pass"
-                                                    type="password"
-                                                    class="w-11/12 focus:outline-none focus:text-gray-600 p-2 ml-4"
-                                                    placeholder="Nueva"
-                                            />
+
+                                <div class="md:w-5/12 w-full md:pl-9 max-w-sm mx-auto space-y-5 md:inline-flex pl-2">
+                                    <div class="w-full inline-flex border-b">
+                                        <div class="w-1/12 pt-2">
+                                            <svg
+                                                fill="none"
+                                                class="w-6 text-gray-400 mx-auto"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                            >
+                                                <path
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                                                />
+                                            </svg>
                                         </div>
+                                        <input
+                                            name="new_pass"
+                                            type="password"
+                                            class="w-11/12 focus:outline-none focus:text-gray-600 p-2 ml-4"
+                                            placeholder="Nueva"
+                                        />
                                     </div>
-                                    <!-- Boton actualizar-->
-                                    <div class="md:w-3/12 text-center md:pl-6">
-                                        <button name="actualizar" type="submit" class="text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-2 inline-flex items-center focus:outline-none md:float-right">
-                                            <i class="fas fa-sync mr-2"></i>
-                                            Actualizar
-                                        </button>
-                                    </div>
-                                </form>
+                                </div>
+                                <!-- Boton actualizar-->
+                                <div class="md:w-3/12 text-center md:pl-6">
+                                    <button name="actualizar" type="submit" class="text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-2 inline-flex items-center focus:outline-none md:float-right">
+                                        <i class="fas fa-sync mr-2"></i>
+                                        Actualizar
+                                    </button>
+                                </div>
                             </div>
 
                             <hr/>
@@ -262,16 +187,16 @@ if ($info != "") {
                             <div class="w-full p-4 text-right text-gray-500 ">
                                 <button class="inline-flex items-center focus:outline-none mr-4 hover:text-red-400">
                                     <svg
-                                            fill="none"
-                                            class="w-4 mr-2"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
+                                        fill="none"
+                                        class="w-4 mr-2"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
                                     >
                                         <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                stroke-width="2"
-                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                                         />
                                     </svg>
                                     Borrar cuenta
