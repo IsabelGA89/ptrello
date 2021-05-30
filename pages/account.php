bc7d5ac0ee141d62f01ca56bafa02290eb52f78e
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-30 17:43:42 +0200

    Update account.php

--- pages/account.php
+++ pages/account.php
@@ -167,8 +167,7 @@ if(isset($_GET['delete']) && ($_GET['delete']=="true")){
         <!--Info App-->
         <a class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-800"
            href="../index.php">
-            <button type="button" data-title='This is right tooltip' data-placement="right" class="bg-blue-600 text-gray-200 rounded hover:bg-blue-500 px-4 py-2 focus:outline-none">Right</button>
-            <i class="fas fa-home fa-2x"></i>
+            <i  data-title='This is right tooltip' data-placement="right" class=" fas fa-home fa-2x bg-blue-600 text-gray-200 rounded hover:bg-blue-500 px-4 py-2 focus:outline-none"></i>
         </a>
         <!--Quien Soy-->
         <a class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-800"
