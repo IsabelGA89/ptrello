95bcd9e3412ca7c3d2f3c392259258ade2dfacbc
IsabelGA89 <isabelgonzalezanzano@gmail.com>
2021-05-31 18:14:25 +0200

    Update account.php

--- pages/account.php
+++ pages/account.php
@@ -430,67 +430,6 @@ if (isset($_GET['delete']) && ($_GET['delete'] == "true")) {
                             </div>
 
                             <hr/>
-                            <div class="md:inline-flex space-y-4 md:space-y-0 w-full p-4 text-gray-500 items-center">
-                                <h2 class="md:w-1/3 max-w-sm mx-auto">Cuenta</h2>
-                                <!--Email-->
-                                <div class="md:w-2/3 max-w-sm mx-auto">
-                                    <label class="text-sm text-gray-400">Email</label>
-                                    <div class="w-full inline-flex border">
-                                        <div class="pt-2 w-1/12 bg-gray-100 bg-opacity-50">
-                                            <svg
-                                                    fill="none"
-                                                    class="w-6 text-gray-400 mx-auto"
-                                                    viewBox="0 0 24 24"
-                                                    stroke="currentColor"
-                                            >
-                                                <path
-                                                        stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                                                />
-                                            </svg>
-                                        </div>
-                                        <input
-                                                type="email"
-                                                class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
-                                                placeholder="<?= $email ?>"
-                                                disabled
-                                        />
-                                    </div>
-
-                                    <form action="account.php" method="post">
-                                        <!--Username-->
-
-                                        <label class="text-sm mt-5 text-gray-400">Username</label>
-                                        <div class="w-full inline-flex border">
-                                            <div class="pt-2 w-1/12 bg-gray-100 bg-opacity-50">
-                                                <svg
-                                                        fill="none"
-                                                        class="w-6 text-gray-400 mx-auto"
-                                                        viewBox="0 0 24 24"
-                                                        stroke="currentColor"
-                                                >
-                                                    <path
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                                                    />
-                                                </svg>
-                                            </div>
-                                            <input
-                                                    name="new_username"
-                                                    type="text"
-                                                    class="w-11/12 focus:outline-none focus:text-gray-600 p-2"
-                                                    placeholder="<?= $username ?>"
-                                            />
-                                        </div>
-
-                                </div>
-                            </div>
-
-                            <hr/>
                             <!--ELiminar cuenta-->
                             <div class="w-full p-4 text-right text-gray-500 ">
                                 <form id="delete_form">
